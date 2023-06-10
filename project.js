bb1ce6b9aff207bfbfb7285a35552dca93bf213f
tol-uno <unot46@gmail.com>
2023-06-09 23:12:32 -0400

    Finished project page mobile css

--- project.js
+++ project.js
@@ -39,31 +39,71 @@ async function renderProjectPage() {
     document.querySelector("#project-title").innerHTML = designerObject["Project Title"];
     // document.querySelector("#project-summary").innerHTML = designerObject["Project Summary"];
     document.querySelector("#project-description").innerHTML = designerObject["Project Description"];
-
-    // Get how many images are in the folder then create a for loop to add them? 
-    //Or just test if image exists before trying to add it
     
     document.querySelector("#hero-image").src = `assets/images/${designerObject["Full Name"]}/image1.jpg`;
 
-    document.querySelector("#images-container").innerHTML = `
-        <img src="assets/images/${designerObject["Full Name"]}/image2.jpg" alt="image 2">
-        <img src="assets/images/${designerObject["Full Name"]}/image3.jpg" alt="image 3">
-        <img src="assets/images/${designerObject["Full Name"]}/image4.jpg" alt="image 4">
-        <img src="assets/images/${designerObject["Full Name"]}/image5.jpg" alt="image 5">
-        <img src="assets/images/${designerObject["Full Name"]}/image6.jpg" alt="image 6">
-        <img src="assets/images/${designerObject["Full Name"]}/image7.jpg" alt="image 7">
-        <img src="assets/images/${designerObject["Full Name"]}/image8.jpg" alt="image 8">
-        <img src="assets/images/${designerObject["Full Name"]}/image9.jpg" alt="image 9">
-        <img src="assets/images/${designerObject["Full Name"]}/image10.jpg" alt="image 10">
-        <img src="assets/images/${designerObject["Full Name"]}/image10.jpg" alt="image 11">
-        <img src="assets/images/${designerObject["Full Name"]}/image10.jpg" alt="image 12">
+
+    var imageContainer = document.querySelector("#images-container");
+
+    function createImages() { // Loops through images and deletes ones that throw errors
+        for (let i = 2; i <= 12; i++) {
+            var img = document.createElement('img');
+            img.src = `assets/images/${designerObject["Full Name"]}/image${i}.jpg`;
+            imageContainer.appendChild(img);
+
+            img.onerror = function() {
+                // image did not load
+                imageContainer.removeChild(imageContainer.lastElementChild)
+            }
+        }
+    }
+
+    createImages();
+
+    // document.querySelector("#images-container").innerHTML = `
+    //     <img src="assets/images/${designerObject["Full Name"]}/image2.jpg" alt="image 2">
+    //     <img src="assets/images/${designerObject["Full Name"]}/image3.jpg" alt="image 3">
+    //     <img src="assets/images/${designerObject["Full Name"]}/image4.jpg" alt="image 4">
+    //     <img src="assets/images/${designerObject["Full Name"]}/image5.jpg" alt="image 5">
+    //     <img src="assets/images/${designerObject["Full Name"]}/image6.jpg" alt="image 6">
+    //     <img src="assets/images/${designerObject["Full Name"]}/image7.jpg" alt="image 7">
+    //     <img src="assets/images/${designerObject["Full Name"]}/image8.jpg" alt="image 8">
+    //     <img src="assets/images/${designerObject["Full Name"]}/image9.jpg" alt="image 9">
+    //     <img src="assets/images/${designerObject["Full Name"]}/image10.jpg" alt="image 10">
+    //     <img src="assets/images/${designerObject["Full Name"]}/image10.jpg" alt="image 11">
+    //     <img src="assets/images/${designerObject["Full Name"]}/image10.jpg" alt="image 12">
+    // `;
 
 
 
-    `;
+
+    document.querySelector("#headshot").src = `assets/images/${designerObject["Full Name"]}/headshot.jpg`;
 
     document.querySelector("#designer-name").innerHTML = designerObject["Full Name"];
-    document.querySelector("#headshot").src = `assets/images/${designerObject["Full Name"]}/headshot.jpg`;
+    document.querySelector("#personal-bio").innerHTML = designerObject["Personal Bio"];
+    document.querySelector("#email").innerHTML = designerObject["Email"];
+    if ("Website" in designerObject) {
+            document.querySelector("#website").innerHTML = designerObject["Website"].replace("https://","");
+            document.querySelector("#website").href = designerObject["Website"];
+        } else {
+            document.querySelector("#website").remove()
+        }
+    
+        if ("Second Website" in designerObject) {
+            document.querySelector("#website2").innerHTML = designerObject["Second Website"].replace("https://","");
+            document.querySelector("#website2").href = designerObject["Second Website"];
+        } else {
+            document.querySelector("#website2").remove()
+        }
+    
+    
+    if ("Instagram" in designerObject) {
+        document.querySelector("#instagram").innerHTML = `Instagram: @${designerObject["Instagram"]}`;
+        document.querySelector("#instagram").href = `https://www.instagram.com/${designerObject["Instagram"]}`;
+    } else {document.querySelector("#instagram").remove();}
+
+    if ("LinkedIn" in designerObject) {document.querySelector("#linkedin").href = designerObject["LinkedIn"];} else {document.querySelector("#linkedin").remove();}
+    
 
 }
 
